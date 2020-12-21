fd809f6986219f154aa2057e9a52fef2ef7fe2eb
Vojislav Andelkovic <vojislavandjelkovic@yahoo.com>
2020-12-21 17:18:59 +0100

    added database

--- Chef-de-ouvre-Deborah/database.py
+++ Chef-de-ouvre-Deborah/database.py
@@ -24,28 +24,23 @@ def create_tables():
             id_cap SERIAL PRIMARY KEY,
             capacity FLOAT8
             );'''
-        
         query_own = '''CREATE TABLE IF NOT EXISTS owners (
             id_owner SERIAL PRIMARY KEY,
             owner_name TEXT
             );'''
-        
         query_fuel = '''CREATE TABLE IF NOT EXISTS fueltypes (
             id_fuel SERIAL PRIMARY KEY,
             fuel_type TEXT
             );'''
-        
         query_loc = '''CREATE TABLE IF NOT EXISTS localisations (
             id_loc SERIAL PRIMARY KEY,
             country_name TEXT
             );'''
-        
         query_geo = '''CREATE TABLE IF NOT EXISTS geolocalisations (
             id_geo SERIAL PRIMARY KEY,
             latitude FLOAT8,
             longitude FLOAT8
             );'''
-        
         query_ppn = '''CREATE TABLE IF NOT EXISTS ppnames (
             id_ppn SERIAL PRIMARY KEY,
             name TEXT,
@@ -53,15 +48,28 @@ def create_tables():
             id_geo INTEGER,
             FOREIGN KEY(id_geo) REFERENCES geolocalisations(id_geo)
             );'''
-            
+        # query_pow = '''CREATE TABLE IF NOT EXISTS powerplants (
+        #     id SERIAL PRIMARY KEY,
+        #     id_name INTEGER,
+        #     id_capacity INTEGER,
+        #     id_localisation INTEGER,
+        #     id_owner INTEGER,
+        #     id_fuel INTEGER,
+        #     FOREIGN KEY(id_name) REFERENCES ppnames(id_ppn),
+        #     FOREIGN KEY(id_capacity) REFERENCES capacities(id_cap),
+        #     FOREIGN KEY(id_localisation) REFERENCES localisations(id_loc),
+        #     FOREIGN KEY(id_owner) REFERENCES owners(id_owner),
+        #     FOREIGN KEY(id_fuel) REFERENCES fueltypes(id_fuel)
+        #     );'''
         query_pow='''CREATE TABLE IF NOT EXISTS powerplants (
-            id_cap integer references capacities,
-            id_owner integer references owners,
-            id_fuel integer references fueltypes,
-            id_loc integer references localisations,
-            id_ppn integer references ppnames,
-            constraint pk primary key (id_cap ,id_owner, id_fuel,id_loc, id_ppn)
-            );'''
+                id_cap integer references capacities,
+                id_owner integer references owners,
+                id_fuel integer references fueltypes,
+                id_loc integer references localisations,
+                id_ppn integer references ppnames,
+                
+                constraint pk primary key (id_cap ,id_owner, id_fuel,id_loc, id_ppn)
+                );'''
                 
         cursor.execute(query_cap)
         cursor.execute(query_own)
@@ -127,3 +135,13 @@ def insert_ppn(name, commissioning_year):
         print("CSV data imported")
     except e:
         print("Could not insert data")
+
+        
+    #     def insert_pow():
+    # # try:
+    # #     data = csv.reader(open('/home/voja/Desktop/Final project/.csv'),delimiter=',')
+    # #     for row in data:
+    # #         cursor.execute('''INSERT INTO power_plants () VALUES (%s,%s);''', row)
+    # #     print("CSV data imported")
+    # # except e:
+    # #     print("Could not insert data")
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import database
 
-#database.connect_db("test", "md", "postgres", "localhost", "5432")
+#database.connect_db("test", "energy", "ecolo", "localhost", "5432")
 
 database.create_tables()
+# database.insert_geo("latitude", "longitude")
+database.insert_ppn("name", "commissioning_year")
+# database.insert_cap()
+# database.insert_own()
+# database.insert_fuel()
+# database.insert_loc()
