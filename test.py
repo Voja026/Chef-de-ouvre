5540c9b12431097733440a050b04ca1612a61f73
Vojislav Andelkovic <vojislavandjelkovic@yahoo.com>
2020-12-23 19:28:00 +0100

    added

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import database
 #database.connect_db("test", "energy", "ecolo", "localhost", "5432")
 
 database.create_tables()
-# database.insert_geo("latitude", "longitude")
-database.insert_ppn("name", "latitude","longitude","commissioning_year")
+#database.insert_ppn("name", "latitude","longitude","commissioning_year")
 # database.insert_cap()
 # database.insert_own()
 # database.insert_fuel()
